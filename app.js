d4f3ab6713ade9f8f2ae9b5b1126cc8a140d2ebe
lanin0777 <73796876+lanin0777@users.noreply.github.com>
2020-12-04 23:59:10 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,52 @@
+const CORS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+  'Access-Control-Allow-Headers':
+    'x-test,Content-Type,Accept, Access-Control-Allow-Headers',
+};
+
+function myFunction(express, bodyParser, createReadStream, crypto, http) {
+  const app = express();
+
+  app.port = process.env.PORT || 4321;
+
+  app
+    .use((req, res, next) => {
+      res.set(CORS);
+      next();
+    })
+    .use(bodyParser.urlencoded({ extended: true }))
+    .get('/sha1/:input', (req, res) => {
+      let hash = crypto.createHash('sha1');
+      hash.update(req.params.input);
+      res.send(hash.digest('hex'));
+    })
+
+    .get('/login/', (req, res) => res.send('alexlaikn'))
+    .get('/code/', (req, res) => {
+      let filename = import.meta.url.substring(9);
+      createReadStream(filename).pipe(res);
+    });
+
+  app.all('/req/', (req, res) => {
+    let url = req.query.addr;
+    http.get(url, (response) => {
+      let data = '';
+      response.on('data', (chunk) => (data += chunk));
+      response.on('end', () => {
+        res
+          .set({
+            'Content-Type': 'text/plain; charset=utf-8',
+          })
+          .end(data);
+      });
+    });
+  });
+  app.all('*', (req, res) => {
+    res.send('alexlaikn');
+  });
+  return app;
+}
+
+export default myFunction;
+
